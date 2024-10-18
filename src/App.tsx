9c66ed5f86d667c81de9dc4c736b44ef211b1395
Vinicius <viniciusdosanjos63@gmail.com>
2024-10-18 20:31:36 -0300

    feat: Appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import "./App.css";
+import AppBar from "./components/AppBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Card sx={{ margin: 2 }}>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Bahamas
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card >
-      <Button variant="contained">Botao</Button>
+      <AppBar></AppBar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
